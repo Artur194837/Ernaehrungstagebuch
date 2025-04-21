17e40b2c1f466ab7075e74929286e4c0c373dc50
Artur Krasnizki <xunleashed1@gmail.com>
2025-04-21 15:07:00 +0200

    Name geändert

--- tagebuch/src/main/java/ernaehrungstagebuch/tagebuch/NahrungsmittelRepository.java
+++
@@ -1,9 +0,0 @@
-package ernaehrungstagebuch.tagebuch;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NahrungsmittelRepository extends JpaRepository<Nahrungsmittel, Long> {
-
-}
